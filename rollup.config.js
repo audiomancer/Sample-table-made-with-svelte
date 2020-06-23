515be98388ad372a6c5fe6cccc0bbc8d41e77cc9
audiomancer <audiomancer@bluewin.ch>
2020-06-23 11:19:23 +0200

    Github pages final fix

--- rollup.config.js
+++ rollup.config.js
@@ -31,10 +31,10 @@ export default {
 		sourcemap: production ? false : true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'docs/build/bundle.js'
 	},
 	plugins: [
-		del({ targets: 'public/build/*', runOnce: true }),
+		del({ targets: 'docs/build/*', runOnce: true }),
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
@@ -51,7 +51,7 @@ export default {
 			}),
 
 			css: (css) => {
-				css.write('public/build/bundle.css', production ? false : true)
+				css.write('docs/build/bundle.css', production ? false : true)
 			}
 		}),
 
@@ -72,9 +72,9 @@ export default {
 		// the bundle has been generated
 		!production && serve(),
 
-		// Watch the `public` directory and refresh the
+		// Watch the `docs` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('docs'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
